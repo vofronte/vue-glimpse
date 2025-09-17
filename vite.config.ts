6a9ead126f3654360210f59b86d0b2dc7c72a10b
Vladimir Sergeev <52858217+vofronte@users.noreply.github.com>
2025-09-18 00:23:24 +0700

    build(vite): configure conditional dev/prod builds

--- vite.config.ts
+++ vite.config.ts
@@ -1,34 +1,44 @@
 import { builtinModules } from 'node:module'
-import process from 'node:process'
 import { defineConfig } from 'vitest/config'
 
-const builtins = builtinModules.filter(m => !m.startsWith('_'))
-const builtinsWithNodePrefix = builtins.map(m => `node:${m}`)
+const builtins = builtinModules.filter(m => !m.startsWith('_')).map(m => `node:${m}`)
+
+const devExternal = [
+  '@vue/compiler-sfc',
+  '@vue/compiler-dom',
+  'typescript',
+]
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  test: {
-    globals: true,
-    environment: 'node',
-    include: ['tests/**/*.test.ts'],
-  },
-  build: {
-    outDir: 'out',
-    emptyOutDir: true,
-    lib: {
-      entry: 'src/extension.ts',
-      formats: ['cjs'],
-      fileName: 'extension',
+export default defineConfig(({ mode }) => {
+  const isProduction = mode === 'production'
+
+  return {
+    test: {
+      globals: true,
+      environment: 'node',
+      include: ['tests/**/*.test.ts'],
     },
-    rollupOptions: {
-      external: [
-        'vscode',
-        ...builtins,
-        ...builtinsWithNodePrefix,
-      ],
+    build: {
+      outDir: 'out',
+      emptyOutDir: true,
+      lib: {
+        entry: 'src/extension.ts',
+        formats: ['cjs'],
+        fileName: 'extension',
+      },
+
+      sourcemap: !isProduction,
+
+      minify: isProduction,
+      rollupOptions: {
+        external: [
+          'vscode',
+          ...builtins,
+          ...(isProduction ? [] : devExternal),
+        ],
+      },
+      target: 'node16',
     },
-    target: 'node16',
-    minify: process.env.NODE_ENV === 'production',
-    sourcemap: true,
-  },
+  }
 })
